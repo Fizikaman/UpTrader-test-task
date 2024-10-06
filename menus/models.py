4227c46a53f932af96c9ea337cac7b7d1cb10476
Fizikaman <fizikaman26@gmail.com>
2024-10-06 20:39:52 +0300

    models menus

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.urls import reverse, NoReverseMatch
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(
+        Menu, related_name='items', on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey(
+        'self', null=True, blank=True, related_name='children', on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=200, blank=True)
+    named_url = models.CharField(max_length=200, blank=True)
+    order = models.PositiveIntegerField(default=0)
+
+    def get_absolute_url(self):
+        if self.named_url:
+            try:
+                return reverse(self.named_url)
+            except NoReverseMatch:
+                return '#'
+        elif self.url:
+            return self.url
+        else:
+            return '#'
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Пункт меню'
+        verbose_name_plural = 'Пункты меню'
