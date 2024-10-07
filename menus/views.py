46cd8c132f5db7b8734fbdb2ab32f319f5cfe7d6
Fizikaman <fizikaman26@gmail.com>
2024-10-07 23:12:08 +0300

    views

--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Menu
+from .serializers import MenuSerializer
 
-# Create your views here.
+
+class MenuViewSet(viewsets.ViewSet):
+    """Вьюха для вывода иерархического меню"""
+
+    def retrieve(self, request, pk=None):
+        try:
+            menu = Menu.objects.get(name=pk)
+        except Menu.DoesNotExist:
+            return Response({'detail': 'Menu not found.'}, status=404)
+
+        serializer = MenuSerializer(menu, context={'request': request})
+
+        return render(request, 'base.html', {'menu': serializer.data})
