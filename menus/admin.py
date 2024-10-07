988e99b0b5aa989e387ed7b7dee871529b2e3fd6
Fizikaman <fizikaman26@gmail.com>
2024-10-07 23:11:24 +0300

    admin

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Menu, MenuItem
 
-# Register your models here.
+
+class MenuItemInline(admin.TabularInline):
+    model = MenuItem
+    fields = ('title', 'parent', 'url', 'named_url', 'order')
+    extra = 1
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    inlines = [MenuItemInline]
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'menu', 'parent', 'order')
+    list_filter = ('menu',)
+    ordering = ('menu', 'order')
